de76079c23812ad1de696c22a98c1224f96a6a66
Varsha022 <varsha.purswani@rhsmith.umd.edu>
2017-03-13 17:23:21 -0400

    Update Catering.py

--- Catering.py
+++ Catering.py
@@ -1,1 +1,14 @@
 #Main file
+from tkinter import filedialog
+from tkinter import *
+import pandas
+print("Varsha")
+
+root = Tk()
+root.fileName = filedialog.askopenfilename( filetypes = (("All files","*.xlsx"),))
+
+print("err:",root.fileName)
+
+
+df = pandas.ExcelFile(root.fileName)
+df.sheet_names
