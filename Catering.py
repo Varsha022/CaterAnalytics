ea7b2793fd2eee14d680be4eeb508868895b82e3
Varsha022 <varsha.purswani@rhsmith.umd.edu>
2017-03-15 13:09:29 -0400

    Update Catering.py

--- Catering.py
+++ Catering.py
@@ -1,1 +1,15 @@
 #Main file
+from tkinter import filedialog as fd
+from tkinter import *
+import pandas as pd
+from pandas import DataFrame, read_csv
+import matplotlib
+import sys
+
+# opens a filedialog
+filename = fd.askopenfilename()
+location = str(filename)
+
+# convert the contents of that file in a df
+df = pd.read_csv(location)
+print(df)
